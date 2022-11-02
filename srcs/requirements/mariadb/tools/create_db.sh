c7259e65b254e2b060875c4a11d5be259b19b15b
malbrand <maeva.albrand@icloud.com>
2022-11-02 11:55:31 +0100

    mariadb ok

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
+sleep 1
 
-service mysql start -u root -p$MYSQL_ROOT_PASSWORD
+service mysql start
+
+sleep 1
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATA}\`;"
+
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATA}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+sleep 1
+
 mysqld_safe -u root -p$MYSQL_ROOT_PASSWORD
+
+sleep 1
 #echo "\e[0;32mBLOPBLOP\e[0m"
