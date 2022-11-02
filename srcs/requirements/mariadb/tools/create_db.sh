5306571b688ebc791b5f3ad175ce88aae77e7383
malbrand <maeva.albrand@icloud.com>
2022-11-02 19:41:25 +0100

    test wp

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -6,6 +6,12 @@ service mysql start
 
 sleep 1
 
+if [ -d "/var/lib/mysql/$MYSQL_DATA" ]
+then
+
+	echo "Database already exists"
+else
+
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATA}\`;"
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -23,4 +29,6 @@ sleep 1
 mysqld_safe -u root -p$MYSQL_ROOT_PASSWORD
 
 sleep 1
-#echo "\e[0;32mBLOPBLOP\e[0m"
+
+fi 
+
