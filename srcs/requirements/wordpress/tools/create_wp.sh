669df0b0855d9f29c76467d717b2c5d183ffb3f6
Administrateur <maeva.albrand@icloud.com>
2022-11-03 19:35:48 +0100

    wp test

--- srcs/requirements/wordpress/tools/create_wp.sh
+++ srcs/requirements/wordpress/tools/create_wp.sh
@@ -4,17 +4,10 @@ if [ -f ./wordpress/wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-	cd /var/www/html/
-	#Download wordpress
 	wget https://wordpress.org/latest.tar.gz
 	tar -xzvf latest.tar.gz
 	rm -rf latest.tar.gz
 
-	#Update configuration file
-	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	#mv ./www.conf /etc/php/7.3/fpm/pool.d/
-
-	#Inport env variables in the config file
 	cd /var/www/html/wordpress
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
