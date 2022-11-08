724dc1c7d440b88282c0c40d483c3d4e3712e73e
malbrand <maeva.albrand@icloud.com>
2022-11-08 16:02:45 +0100

    docker-compose ok mariadb exit code 0

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 
-sleep 1
-
-service mysql start
-
-sleep 1
 
 if [ -d "/var/lib/mysql/$MYSQL_DATA" ]
 then
 
-	echo "Database already exists"
+echo "\e[32mDatabase already exists\e[0m"
+
 else
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATA}\`;"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATA}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
+sleep 1
+service mysql start
 sleep 1
 
-mysqld_safe -u root -p$MYSQL_ROOT_PASSWORD
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATA}\`;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATA}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
+sleep 1
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+sleep 1
+
+mysqld_safe
 sleep 1
 
 fi 
-
