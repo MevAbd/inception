b1cee47d9d9a700cb803c61ecbf607c9ed5ede16
malbrand <maeva.albrand@icloud.com>
2022-11-24 17:43:39 +0100

    ...

--- srcs/requirements/wordpress/tools/create_wp.sh
+++ srcs/requirements/wordpress/tools/create_wp.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-wordpressPath=/var/www/html/wordpress
-
-path=$wordpressPath
+wpPath=/var/www/html/wordpress
 url="malbrand.42.fr"
 title="inception"
 admin_user=$MYSQL_ADMIN
@@ -11,12 +9,12 @@ admin_email=$MYSQL_MAIL
 
 function wp-cli() {
 wp --allow-root core install \
-		--path=$1 \
-		--url=$2/ \
-		--title=$3 \
-		--admin_user=$4 \
-		--admin_password=$5 \
-		--admin_email=$6	
+		--path=$wpPath \
+		--url=$url/ \
+		--title=$title \
+		--admin_user=$admin_user \
+		--admin_password=$admin_password \
+		--admin_email=$admin_email	
 }
 
 
@@ -25,9 +23,8 @@ mv wp-cli.phar /usr/bin/wp
 chmod 777 /usr/bin/wp
 
 rm -rf $wordpressPath;
-wp core download --allow-root --path=$wordpressPath;
-cp /root/wp-config.php $wordpressPath;
-wp-cli $path $title $admin_user $admin_password $admin_email;
-
+wp core download --allow-root --path=/var/www/html/wordpress;
+cp /root/wp-config.php /var/www/html/wordpress;
+wp --allow-root core install --path=/var/www/html/wordpress --url=https://malbrand.fr --title=inception --admin_user=malbrand --admin_password=sqlokapi --admin_email=exemple@exemple.fr;
 
 /usr/sbin/php-fpm7.3 -F
